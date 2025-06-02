1b3664d943f4d17c2277a4400bd3bf6e720eb38a
Taurai Gombera <tgombera@nfb.mw>
2025-06-02 17:59:51 +0200

    loading env variables from .env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+	"simple-blogging/internal/env"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
